759c0b3e5a226b9e8d269a1a2e3f9cc4ea75d099
sts_user <sts_user@neuedu.com>
2020-03-02 11:28:52 +0800

    my first commit in eclipse

---
+++ src/main/java/com/neuedu/git/Apple.java
@@ -0,0 +1,5 @@
+package com.neuedu.git;
+
+public class Apple {
+
+}
---
+++ src/test/java/com/neuedu/git/test/GoodTest.java
@@ -0,0 +1,14 @@
+package com.neuedu.git.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GoodTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
